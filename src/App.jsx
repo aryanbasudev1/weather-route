318456b422fec3ede4acc40b11930d06f74b3ebe
aryanbasudev1 <aryanbasudev1@gmail.com>
2024-05-26 23:01:36 +0530

    Added recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import "./App.css";
-import { useEffect } from "react";
-import axios from "axios";
 import SearchBox from "./components/SearchBox";
 
 function App() {
